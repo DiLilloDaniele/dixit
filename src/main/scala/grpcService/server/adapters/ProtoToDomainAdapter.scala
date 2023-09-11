509211f705295269b4b3dab0c679af95105897b2
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-09-11 17:30:01 +0200

    chore: added proto to domain adapter

---
+++ src/main/scala/grpcService/server/adapters/ProtoToDomainAdapter.scala
@@ -0,0 +1,10 @@
+package grpcService.server.adapters
+
+import grpcService.server.domain.model.Game
+import grpcService.{LoginRequest, OpenedGames}
+
+object ProtoToDomainAdapter:
+  
+  def protoToUserCredentials(req: LoginRequest): Tuple2[String, String] = (req.name, req.password)
+
+  def protoToGames(game: Set[Game]): OpenedGames = OpenedGames(game.map(game => game.address).toSeq)
