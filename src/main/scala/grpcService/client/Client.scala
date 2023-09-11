2d0cec4c2bc41e18cc9e041a1953aff68360ebba
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-09-11 17:29:17 +0200

    feat: added grpc client implementation

--- src/main/scala/grpcService/client/Client.scala
+++ src/main/scala/grpcService/client/Client.scala
@@ -2,8 +2,12 @@ package grpcService.client
 
 import akka.actor.ActorSystem
 import akka.grpc.GrpcClientSettings
-import grpcService.{HelloMessage, Server, ServerClient}
-import scala.util.{ Failure, Success }
+import grpcService.{HelloMessage, LoginRequest, OpenedGamesRequest, Server, ServerClient}
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.{Failure, Success}
+import grpcService.client.controller.GameControllerObj
+import grpcService.client.controller.GameControllerObj.SuccessFun
 
 object Client {
   def main(args: Array[String]): Unit = {
@@ -12,7 +16,7 @@ object Client {
     implicit val ec = sys.dispatcher
 
     // Configure the client by code:
-    val clientSettings = GrpcClientSettings.connectToServiceAt("127.0.0.1", 8080).withTls(false)
+    val clientSettings = GrpcClientSettings.connectToServiceAt("127.0.0.1", 8083).withTls(false)
 
     // Or via application.conf:
     // val clientSettings = GrpcClientSettings.fromConfig(GreeterService.name)
@@ -28,4 +32,42 @@ object Client {
         println(s"Error sayHello: $e")
     }
   }
+}
+
+class ClientImpl() {
+ 
+  implicit val sys: ActorSystem = ActorSystem("HelloWorldClient")
+  implicit val ec: ExecutionContextExecutor = sys.dispatcher
+
+  val clientSettings = GrpcClientSettings.connectToServiceAt("127.0.0.1", 8083).withTls(false)
+
+  val client: Server = ServerClient(clientSettings)
+
+  def getAvailableGames(success: SuccessFun[List[String]]) =
+    val reply = client.getOpenedGames(OpenedGamesRequest())
+    reply.onComplete {
+      case Success(msg) =>
+        success(msg.clusterName.toList)
+      case Failure(e) =>
+        println(s"Error sayHello: $e")
+    }
+    
+  def login(username: String, password: String, success: SuccessFun[Boolean]) =
+    val reply = client.login(LoginRequest(username, password))
+    reply.onComplete {
+      case Success(msg) =>
+        success(msg.response)
+      case Failure(e) =>
+        println(s"Error sayHello: $e")
+    }
+    
+  def register(username: String, password: String, success: SuccessFun[Boolean]) =
+    val reply = client.register(LoginRequest(username, password))
+    reply.onComplete {
+      case Success(msg) =>
+        success(msg.response)
+      case Failure(e) =>
+        println(s"Error sayHello: $e")
+    }
+
 }
