e0bcb995e3c47de9527e9d36971e6621f7227c69
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-09-11 17:27:01 +0200

    feat: added new utils for environment manual testing

--- src/main/scala/grpcService/client/actors/utils.scala
+++ src/main/scala/grpcService/client/actors/utils.scala
@@ -1,19 +1,25 @@
+package grpcService.client.actors
+
+import akka.actor.typed.receptionist.Receptionist
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
 import akka.cluster.Cluster
 import com.typesafe.config.ConfigFactory
+import grpcService.client.actors.behaviors.{ForemanBehavior, ForemanBehaviorImpl}
 
 import java.io.{BufferedReader, InputStreamReader}
 
 object utils {
 
   case class Request(replyTo: ActorRef[Message]) extends Message
+
   case object Reply extends Message
 
+  //spawn di Foreman, cluster listener e gui
   def apply(): Behavior[Message] = Behaviors.setup { ctx =>
     val cluster = Cluster(ctx.system)
 
-    Behaviors.receiveMessage {
+    Behaviors.receiveMessage[Message] {
       case Request(replyTo) =>
         ctx.log.info("REQUEST RECEIVED")
         replyTo ! Reply
@@ -21,26 +27,34 @@ object utils {
       case Reply =>
         ctx.log.info("REPLY RECEIVED")
         Behaviors.same
+    }.receiveSignal { case (ctx, t @ Terminated(_)) =>
+      ctx.log.info("System terminated. Shutting down")
+      Behaviors.stopped
     }
 
   }
 
   def startupWithRole[X](role: String, port: String, ip: String)(root: => Behavior[X]): ActorSystem[X] =
     val config = ConfigFactory
-      .parseString(s"""
+      .parseString(
+        s"""
         akka.remote.artery.canonical.hostname=$ip
         akka.remote.artery.canonical.port=$port
         akka.cluster.roles = [$role]
         """)
       .withFallback(ConfigFactory.load("base-cluster"))
 
-      // Create an Akka system
-      ActorSystem(root, "ClusterSystem", config)
+    // Create an Akka system
+    ActorSystem(root, "ClusterSystem", config)
 
   @main def startSeedNode() =
-    startupWithRole("seed", "2551", "192.168.1.118")(apply());
+    startupWithRole("seed", "2551", "192.168.1.118")(ForemanBehavior());
 
   @main def createListener() = startupWithRole("listener", "2552", "192.168.1.118")(apply());
+
+  @main def createActor1() = startupWithRole("actor1", "2553", "192.168.1.118")(apply());
+
+  @main def createActor2() = startupWithRole("actor2", "2554", "192.168.1.118")(apply());
 
   @main def main() =
 
@@ -50,10 +64,8 @@ object utils {
 
     val port: String = reader.readLine();
 
-    System.out.print(ipAddress + ":"+ port)
-    val sender = startupWithRole("sender", port, ipAddress)(apply());
-
-    
+    System.out.print(ipAddress + ":" + port)
+    val sender = startupWithRole("sender", port, ipAddress)(ForemanBehavior());
 
 
 }
