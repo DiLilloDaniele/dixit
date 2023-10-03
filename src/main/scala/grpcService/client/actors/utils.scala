1ec42d45e0d9dc85e59a23e000e576f64245e13e
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-10-03 17:11:30 +0200

    chore: added utils method for testing

--- src/main/scala/grpcService/client/actors/utils.scala
+++ src/main/scala/grpcService/client/actors/utils.scala
@@ -1,23 +1,27 @@
 package grpcService.client.actors
 
 import akka.actor.typed.receptionist.Receptionist
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
 import akka.cluster.Cluster
 import com.typesafe.config.ConfigFactory
-import grpcService.client.actors.behaviors.{ForemanBehavior, ForemanBehaviorImpl}
+import grpcService.client.actors.behaviors.{ForemanBehavior, ForemanBehaviorImpl, PlayerBehavior}
 
 import java.io.{BufferedReader, InputStreamReader}
 
-object utils {
+object utils:
 
   case class Request(replyTo: ActorRef[Message]) extends Message
 
   case object Reply extends Message
 
+
   //spawn di Foreman, cluster listener e gui
   def apply(): Behavior[Message] = Behaviors.setup { ctx =>
-    val cluster = Cluster(ctx.system)
+    val cluster: Cluster = Cluster(ctx.system)
+
+    val testActor = ctx.spawn(Behaviors.setup[Message](context => new ActorClass(context, ctx.self)), "player")
+    testActor ! Request(ctx.self)
 
     Behaviors.receiveMessage[Message] {
       case Request(replyTo) =>
@@ -33,6 +37,8 @@ object utils {
     }
 
   }
+
+
 
   def startupWithRole[X](role: String, port: String, ip: String)(root: => Behavior[X]): ActorSystem[X] =
     val config = ConfigFactory
@@ -56,6 +62,12 @@ object utils {
 
   @main def createActor2() = startupWithRole("actor2", "2554", "192.168.1.118")(apply());
 
+  @main def test() =
+    val foreman = startupWithRole("foreman", "2551", "127.0.0.1")(ForemanBehavior());
+    startupWithRole("listener", "2552", "127.0.0.1")(ClusterListener(foreman));
+    Thread.sleep(1000)
+    startupWithRole("player", "2554", "127.0.0.1")(PlayerBehavior());
+
   @main def main() =
 
     val reader: BufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -68,4 +80,17 @@ object utils {
     val sender = startupWithRole("sender", port, ipAddress)(ForemanBehavior());
 
 
-}
+import utils.Request
+import utils.Reply
+class ActorClass(context: ActorContext[Message], root: ActorRef[Message]) extends AbstractBehavior[Message](context):
+
+  override def onMessage(msg: Message): Behavior[Message] = msg match {
+    case Request(ref) =>
+      context.log.info("request")
+      root ! Reply
+      Behaviors.same
+    case _ =>
+      context.log.info("unknown message")
+      Behaviors.same
+
+  }
