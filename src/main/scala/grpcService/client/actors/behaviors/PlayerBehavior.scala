a7212becc40e942d396436eeaaca1eda1ce70608
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-10-03 17:13:15 +0200

    feat: added new choose and guess behaviors for player actors

--- src/main/scala/grpcService/client/actors/behaviors/PlayerBehavior.scala
+++ src/main/scala/grpcService/client/actors/behaviors/PlayerBehavior.scala
@@ -7,6 +7,8 @@ import grpcService.client.actors.Message
 import grpcService.client.actors.behaviors.PlayerBehavior.*
 import grpcService.client.actors.behaviors.ForemanBehavior
 
+import concurrent.duration.DurationInt
+import scala.language.postfixOps
 import scala.util.Random
 
 object PlayerBehavior:
@@ -15,8 +17,10 @@ object PlayerBehavior:
 
   sealed trait Command extends Message
   //entry messages
+  case class AskToEnter(replyTo: ActorRef[PlayersManagerBehavior.Command]) extends Command
   case object MemberOK extends Command
   case object MemberKO extends Command
+  case object Start extends Command
   case class TurnOf(val player: String) extends Command
 
   //turn messages
@@ -24,18 +28,38 @@ object PlayerBehavior:
   case object NotYourTurn extends Command
 
   //guess messages
-  case class CardChoosen(val title: String, val replyTo: ActorRef[ForemanBehavior.Command]) extends Command
+  case class CardChoosenByOther(val title: String, val replyTo: ActorRef[ForemanBehavior.Command]) extends Command
+  case class CardsSubmittedByOthers(val cards: List[String]) extends Command
+  case class CardRevealed(val cardId: String) extends Command
+  case class CardChoosenForTurn(val cardId: String, val title: String) extends Command
+  case object EndTurn extends Command
+  case class CardsAssigned(cardsId: List[String]) extends Command
+  case class SingleCardSubmitted(cardId: String) extends Command
+  case class GuessedCardbyUser(cardId: String) extends Command
 
-  //choose messages
-
-  def apply(): Behavior[Command] =
+  def apply(logger: Option[ActorRef[Command]] = Option.empty, interactionExt: Option[ActorRef[InteractionBehavior.Command]] = Option.empty): Behavior[Command] =
     Behaviors.setup[Command] { ctx =>
+      ctx.system.receptionist ! Receptionist.Register(Service, ctx.self)
 
-      val player = ctx.spawn(Behaviors.setup[Command](ctx => new PlayerBehaviorImpl(ctx)), "player")
-      ctx.watch(player)
-
-      Behaviors.receiveMessage[Command] {
+      val interaction = interactionExt match {
+        case Some(ref) =>
+          ctx.log.info("INTERACTION ESTERNO")
+          ref
         case _ =>
+          ctx.log.info("INTERACTION INTERNO")
+          ctx.spawn(Behaviors.setup[InteractionBehavior.Command](ctx1 => InteractionGuiImpl(ctx1, ctx.self)), "gui")
+      }
+      val player = ctx.spawn(Behaviors.setup[Command | Receptionist.Listing](context => new PlayerBehaviorImpl(context, interaction, ctx.self)), "player")
+      player ! Start
+      ctx.watch(player)
+      Behaviors.receiveMessage[Command] {
+        case msg: Command =>
+          ctx.log.info("MESSAGGIO RICEVUTO: " + msg)
+          logger match {
+            case Some(ref) => ref ! msg
+            case _ =>
+          }
+          player ! msg
           Behaviors.same
       }.receiveSignal { case (ctx, t @ Terminated(_)) =>
         ctx.log.info("System terminated. Shutting down")
@@ -43,30 +67,81 @@ object PlayerBehavior:
       }
     }
 
-class PlayerBehaviorImpl(context: ActorContext[Command]) extends AbstractBehavior[Command](context):
+class PlayerBehaviorImpl(context: ActorContext[Command | Receptionist.Listing], interactionActor: ActorRef[InteractionBehavior.Command], rootActor: ActorRef[PlayerBehavior.Command]) extends AbstractBehavior[Command | Receptionist.Listing](context):
 
-  override def onMessage(msg: Command): Behavior[Command] = msg match {
-    case MemberOK => gameOn()
-    case MemberKO => Behaviors.stopped
+  override def onMessage(msg: Command | Receptionist.Listing): Behavior[Command | Receptionist.Listing] = msg match {
+    case msg: Receptionist.Listing =>
+      context.log.info("RECEIVED SOMETHING")
+      val services: List[ActorRef[PlayersManagerBehavior.Command]] = msg.serviceInstances(PlayersManagerBehavior.Service).toList
+      if(!services.isEmpty)
+        context.self ! AskToEnter(services.head)
+      Behaviors.same
+    case Start =>
+      context.system.receptionist ! Receptionist.Subscribe(PlayersManagerBehavior.Service, context.self)
+      Behaviors.same
+    case AskToEnter(replyTo) =>
+      replyTo ! PlayersManagerBehavior.AskEnter(rootActor)
+      Behaviors.same
+    case MemberOK =>
+      context.log.info("MEMBER OK")
+      waitForCards()
+    case MemberKO =>
+      interactionActor ! InteractionBehavior.MessageError("Impossibile entrare")
+      Behaviors.stopped
     case m =>
       context.log.info("Unknown message: " + m.toString)
       Behaviors.same
   }
 
-  def gameOn(): Behavior[Command] = Behaviors.receiveMessage {
-    case TurnOf(player) => ???
-    case YourTurn(replyTo) => 
-      // deve inviare un CardToGuess ad un attore InteractionActor, che può essere 
-      // un attore che sceglie random una carta e la invia qui, o la gui vera e propria
-      ???
-    case CardChoosen(title, replyTo) => ???
+  def waitForCards(): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
+    case CardsAssigned(list) =>
+      //interaction actor deve ricevere le carte
+      context.log.info("Carte ottenute")
+      interactionActor ! InteractionBehavior.CardsAssigned(list)
+      gameOn()
     case _ => Behaviors.same
   }
 
-  def guess(): Behavior[Command] = Behaviors.receiveMessage {
-    case _ => ???
+  def gameOn(): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
+    case YourTurn(replyTo) => 
+      // deve inviare un CardToGuess ad un attore InteractionActor, che può essere 
+      // un attore che sceglie random una carta e la invia qui, o la gui vera e propria
+      context.log.info("SONO IL PLAYER, E' IL MIO TURNO")
+      interactionActor ! InteractionBehavior.ChooseTheCard(context.self)
+      choose(replyTo)
+    case CardChoosenByOther(title, replyTo) =>
+      interactionActor ! InteractionBehavior.GuessCard(rootActor, title)
+      guess(title, replyTo)
+    case _ => Behaviors.same
   }
 
-  def choose(): Behavior[Command] = Behaviors.receiveMessage {
-    case _ => ???
+  def guess(title: String, replyTo: ActorRef[ForemanBehavior.Command]): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
+    case SingleCardSubmitted(card) =>
+      replyTo ! ForemanBehavior.SelectionToApply(card, rootActor)
+      Behaviors.same
+    case CardsSubmittedByOthers(cards) =>
+      interactionActor ! InteractionBehavior.ShowCardsProposed(rootActor, cards, "")
+      choose(replyTo)
+    case _ => Behaviors.same
   }
+
+  def choose(replyTo: ActorRef[ForemanBehavior.Command]): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
+    case CardChoosenForTurn(cardId, title) =>
+      // TODO reset interaction
+      replyTo ! ForemanBehavior.CardToGuess(cardId, title, rootActor)
+      gameOn()
+    case GuessedCardbyUser(cardId) =>
+      replyTo ! ForemanBehavior.GuessSelection(cardId, rootActor)
+      waitForCardRevealed(replyTo)
+    case msg => context.log.info("Unknown message: " + msg)
+      Behaviors.same
+  }
+
+  def waitForCardRevealed(replyTo: ActorRef[ForemanBehavior.Command]): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
+    case CardRevealed(cardId) =>
+      // TODO reset interaction with image revealed (optional)
+      gameOn()
+    case msg => context.log.info("Unknown message: " + msg)
+      Behaviors.same
+  }
+
