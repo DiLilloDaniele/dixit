02cf1e4ae0e62818f0f0464590b9db242f39c8dd
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-10-03 17:14:45 +0200

    test: added new manual testing methods

---
+++ src/test/scala/akka/ManualTesting.scala
@@ -0,0 +1,30 @@
+package akka
+
+import grpcService.client.actors.{ClusterListener, utils}
+import grpcService.client.actors.behaviors.{ForemanBehavior, InteractionBehavior, PlayerBehavior, PlayersManagerBehavior}
+
+object ManualTesting {
+
+  @main def testInteractions() =
+    import grpcService.client.actors.utils
+    val foreman = utils.startupWithRole("foreman", "2551", "127.0.0.1")(ForemanBehavior())
+    Thread.sleep(2000)
+    utils.startupWithRole("player1", "2552", "127.0.0.1")(PlayerBehavior())
+    Thread.sleep(2000)
+    utils.startupWithRole("player2", "2553", "127.0.0.1")(PlayerBehavior())
+    Thread.sleep(2000)
+    utils.startupWithRole("player3", "2554", "127.0.0.1")(PlayerBehavior())
+
+
+  @main def executeForeman() =
+    import grpcService.client.actors.utils
+    val foreman = utils.startupWithRole("foreman", "2551", "127.0.0.1")(ForemanBehavior())
+    utils.startupWithRole("player", "2552", "127.0.0.1")(PlayerBehavior())
+
+  @main def executePlayer1() =
+    utils.startupWithRole("player", "2553", "127.0.0.1")(PlayerBehavior())
+
+  @main def executePlayer2() =
+    utils.startupWithRole("player", "2554", "127.0.0.1")(PlayerBehavior())
+
+}
