f8464bc9e42825c165d3b37fdba85d37bb9b29c0
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2023-12-19 15:34:36 +0100

    feat: added new custom mailbox

--- src/main/scala/grpcService/client/actors/prioritizedQueueExample/MyPriorityActorMailbox.scala
+++ src/main/scala/grpcService/client/actors/prioritizedQueueExample/MyPriorityActorMailbox.scala
@@ -2,27 +2,15 @@ package grpcService.client.actors.prioritizedQueueExample
 import akka.actor.*
 import akka.dispatch.*
 import com.typesafe.config.Config
-import grpcService.client.actors.prioritizedQueueExample.MyPriorityActor.Hello
-import grpcService.client.actors.prioritizedQueueExample.MyPriorityActor.Exit
+import grpcService.client.actors.behaviors.PlayersManagerBehavior.*
 
 class MyPriorityActorMailbox(settings:
     ActorSystem.Settings, config: Config) extends 
     UnboundedPriorityMailbox ( 
     // Create a new PriorityGenerator, lower prio means more important 
     PriorityGenerator { 
-        case Hello(addr) =>
+        case Stop =>
             0
-        case Exit(addr) =>
-            1
-        // Int Messages 
-        case x: Int =>
-            1 
-        // String Messages 
-        case x: String =>
-            0 
-        // Long messages 
-        case x: Long =>
-            2 
         // other messages 
-        case _ => 3 
+        case _ => 1
 })
